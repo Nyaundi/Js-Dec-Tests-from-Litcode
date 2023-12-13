61b9d90bd3098757ae0cab87ce3b825db6e62207
Nyaundi <jamesmabuka260@gmail.com>
2023-12-13 17:14:40 +0300

    added arrays test

--- main.js
+++ main.js
@@ -16,3 +16,14 @@ arr.unshift(0);
 arr.push(6);
 arr.reverse();
 console.log(arr)
+
+// Challenge two : Instructions:
+
+// Combine arr1 and arr2 into a new array called arr3 with the following elements:
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+const arr3 = arr1.slice(0, 4).concat(arr2);
+
+console.log(arr3);
