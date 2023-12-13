57896dbdc0718c08edada827aa06c560d355065d
Nyaundi <jamesmabuka260@gmail.com>
2023-12-13 16:58:06 +0300

    First test

---
+++ main.js
@@ -0,0 +1,18 @@
+// //Event objects
+
+// const logo = document.querySelector('img');
+
+// function onClick(e){
+//    console.log(e.target);
+//    console.log(e.currentTarget);
+
+// };
+
+// logo.addEventListener('click', onClick);
+
+//Arrays Challenge
+const arr = [1, 2, 3, 4, 5];
+arr.unshift(0);
+arr.push(6);
+arr.reverse();
+console.log(arr)
